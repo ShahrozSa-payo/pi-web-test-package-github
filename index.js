19d02216b6f3ef38d8be56ba78c4a9d8f25b807a
Shahroz Saleem <shahrozsa@payoneer.com>
2022-05-31 12:44:00 +0500

    initialize npm package

---
+++ index.js
@@ -0,0 +1,8 @@
+function test(param) {
+  param
+    ? console.log("shahrozsa-payo-test-package-npm param is :: ", param)
+    : console.log("shahrozsa-payo-test-package-npm is working");
+}
+
+module.exports = test;
+
